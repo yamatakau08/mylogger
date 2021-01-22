7c4291598cda59f02d1c65b6ee06e433f0d6a2d1
yamatakau08 <yamatakau08@gmail.com>
2021-01-22 22:05:17 +0900

    .

--- mylogger.rb
+++ mylogger.rb
@@ -1,43 +1,65 @@
-## This class puts out log both STDOUT and YYMMDD_HHMMSS.log
+require 'logger'
+
+=begin
+MEMO
+it might be better to pass the formatter for own purpose
+
+## usage
+
+$logger = Mylogger.new(STDOUT)
+or
+$logger = Mylogger.new("foo.log")
+$logger.info "info test"
+
+or
+module Logging
+  def logger
+    @logger ||= Mylogger.new(STDOUT)
+    # or
+    # @logger ||= Mylogger.new("foo.log")
+  end
+end
+
+include Logging
+logger.info "info test"
+logger.warn "warn test"
+=end
+
+## This class puts out log both in log file and STDOUT
 class Mylogger < Logger
-  def initialize(logfile = nil ,datetime_prefix = nil)
-    logdev = logfile ? logfile : STDOUT
-    formatter = proc do |severity, datetime, progname, msg|
-      ## "#{datetime} [#{severity}] #{progname} #{msg}\n"
+  def initialize(logdev, shift_age = 0, shift_size = 1048576, level: DEBUG,
+                 progname: nil, formatter: nil, datetime_format: nil,
+                 shift_period_suffix: '%Y%m%d')
 
-      ## loft adjustment severity
-      # https://stackoverflow.com/questions/6407141/how-can-i-have-ruby-logger-log-output-to-stdout-as-well-as-file/34026278#34026278
-      nseverity = severity.ljust(5," ")
+    unless formatter
+      # https://stackoverflow.com/a/34026278
+      # formatter https://docs.ruby-lang.org/ja/latest/class/Logger=3a=3aFormatter.html
+      formatter = proc do |severity,time,progname,msg|
 
-      ## for logfile
-      # puts must be executed before formatter definition
-      # ansi color: https://qiita.com/PruneMazui/items/8a023347772620025ad6#%E5%87%BA%E5%8A%9B%E8%89%B2%E3%81%AE%E5%A4%89%E6%9B%B4
-      # \e[30m : character color black
-      # \e[43m : background clolor yellow
-      # \e[0m  : reset ansi escape sequence set before
-      if logfile
-        if datetime_prefix
-          if severity == "ERROR" || severity == "WARN"
-            puts "#{datetime.strftime('%Y-%m-%d %H:%M:%S.%3N')} [\e[30m\e[43m#{nseverity}\e[0m] : #{msg}\n"
-          else
-            puts "#{datetime.strftime('%Y-%m-%d %H:%M:%S.%3N')} [#{nseverity}] : #{msg}\n"
-          end
+        nseverity = severity.ljust(5," ")
+
+        # ansi color: https://qiita.com/PruneMazui/items/8a023347772620025ad6#%E5%87%BA%E5%8A%9B%E8%89%B2%E3%81%AE%E5%A4%89%E6%9B%B4
+        # \e[30m : character color black
+        # \e[43m : background clolor yellow
+        # \e[0m  : reset ansi escape sequence set before
+        # for stdout
+        if severity == "ERROR" || severity == "WARN"
+          # for stdout in case of log file
+          puts "#{time.strftime('%Y-%m-%d %H:%M:%S.%3N')} [\e[30m\e[43m#{nseverity}\e[0m] : #{msg}" if logdev.class == String
+          # for log file
+          "#{time.strftime('%Y-%m-%d %H:%M:%S.%3N')} [\e[30m\e[43m#{nseverity}\e[0m] : #{msg}\n" # need \n
         else
-          puts "#{msg}"
+          # for stdout in case of log file
+          puts "#{time.strftime('%Y-%m-%d %H:%M:%S.%3N')} [#{nseverity}] : #{msg}" if logdev.class == String
+          # for log file
+          "#{time.strftime('%Y-%m-%d %H:%M:%S.%3N')} [#{nseverity}] : #{msg}\n" # need \n
         end
-      end
-
-      # for formatter
-      if date_time_prefix
-        ## formatter and for logfile
-        # https://docs.ruby-lang.org/ja/latest/class/Logger.html#I_FORMATTER--3D
-        # *return value should be STRING of call method*
-        "#{datetime.strftime('%Y-%m-%d %H:%M:%S.%3N')} [#{nseverity}] : #{msg}\n"
-      else
-        puts "#{msg}"
       end
     end
 
-    super(logdev,formatter: formatter)
+    super(logdev, shift_age = 0, shift_size = 1048576, level: DEBUG,
+                 progname: nil, formatter: formatter, datetime_format: nil,
+                 shift_period_suffix: '%Y%m%d')
   end
 end
+
